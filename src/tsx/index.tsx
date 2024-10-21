b342730f054f237717b94dacc6e236db26f7caab
Anderson_Ramos_07 <andersonramos07@live.com>
2024-10-21 20:03:08 -0300

    adding Modal

--- src/tsx/index.tsx
+++ src/tsx/index.tsx
@@ -13,62 +13,69 @@ export default function App() {
     function toTranslate() {
         lang == 1? setLang(0): setLang(1);
     }
+    const [visible, setVisible] = useState(true);
+    const toCloseModal = () => {
+        visible == true? setVisible(false) : setVisible(true)
+    }
     return(
         <SafeAreaView style={styles.container}>
             <LinearGradient
-            colors={['#000007','transparent']}>                
-            <View style={{flex:3}}>
-                <View>
-                    <Text style={[styles.titulo, {fontStyle: 'italic', textTransform: 'uppercase'}]}>
-                        {data.titles[lang]}
-                    </Text>
-                    <Velocimetro/>
-                    <Text style={[styles.texts, {fontStyle: 'italic'}]}>
-                        {data.metrics[lang]}
+                colors={['#000007','transparent']} style={{flex:1}}>                
+                <View style={{flex:3}}>
+                    <View>
+                        <Text style={[styles.titulo,
+                                {fontStyle: 'italic',
+                                    textTransform: 'uppercase',
+                                    textDecorationLine: 'underline',
+                                    textDecorationStyle:'double'}]}>
+                            {data.titles[lang]}
+                        </Text>
+                        <Velocimetro/>
+                        <Text style={[styles.texts, {fontStyle: 'italic'}]}>
+                            {data.metrics[lang]}
+                        </Text>
+                    </View>
+                </View>
+                <View style={[styles.icons, {flex: 2, marginTop: 125}]}>
+                    <MaterialCommunityIcons name="gas-station" size={75} color="#b8860b"/> 
+                    <MaterialCommunityIcons name="oil" size={75} color="#ff1500" />
+                    <MaterialCommunityIcons name="progress-alert" size={75} color="#15ff00" />
+                </View>
+                <View style={styles.buttons}>
+                    <Pressable
+                    onPress={toCloseModal}
+                    style={styles.button}>
+                        <MaterialCommunityIcons name="speedometer" size={25} color="#cfcfcf"/>
+                        <MaterialCommunityIcons name="plus-thick" size={15} color="#cfcfcf"/>
+                    </Pressable>
+
+                    <Pressable
+                    onPress={toCloseModal}
+                    style={styles.button}>
+                        <MaterialCommunityIcons name="tools" size={25} color="#cfcfcf"/>
+                        <MaterialCommunityIcons name="plus-thick" size={15} color="#cfcfcf"/>
+                    </Pressable>
+                </View>
+                <View style={styles.lastsServices}>
+                    <Text style={styles.texts}>
+                        Alertas: ultimos servicos
                     </Text>
                 </View>
-            </View>
-            <View style={[styles.icons, {flex: 2, marginTop: 100}]}>
-                <MaterialCommunityIcons name="gas-station" size={75} color="#b8860b"/> 
-                <MaterialCommunityIcons name="oil" size={75} color="#ff1500" />
-                <MaterialCommunityIcons name="progress-alert" size={75} color="#15ff00" />
-            </View>
-            <View style={styles.buttons}>
+                <Modal
+                    descricao={data.buttons[lang][lang]}
+                    type={data.services[lang][lang]}
+                    isVisible={visible}
+                    onClose={toCloseModal}>
+                </Modal>
                 <Pressable
-                onPress={alerta}
-                style={styles.button}>
-                    <MaterialCommunityIcons name="speedometer" size={25} color="#cfcfcf"/>
-                    <MaterialCommunityIcons name="plus-thick" size={15} color="#cfcfcf"/>
+                    onPress={toTranslate}>
+                <MaterialCommunityIcons
+                    name="translate"
+                    size={25}
+                    color="#cfcfcf"
+                    style={{marginBottom: 10, marginLeft: 10}}/>
                 </Pressable>
-
-                <Pressable
-                onPress={alerta}
-                style={styles.button}>
-                    <MaterialCommunityIcons name="tools" size={25} color="#cfcfcf"/>
-                    <MaterialCommunityIcons name="plus-thick" size={15} color="#cfcfcf"/>
-                </Pressable>
-            </View>
-            <View style={styles.lastsServices}>
-                <Text style={styles.texts}>
-                    Alertas: ultimos servicos
-                </Text>
-            </View>
-            <View
-            style={{opacity:1}}>
-            <Modal
-                descricao={data.metrics[lang]}
-                type={data.services[lang][lang]}>
-            </Modal>
-                    </View>
-            <Pressable
-                onPress={toTranslate}>
-            <MaterialCommunityIcons
-                name="translate"
-                size={25}
-                color="#cfcfcf"
-                style={{marginBottom: 10, marginLeft: 10}}/>
-            </Pressable>
-                </LinearGradient>
+            </LinearGradient>
         </SafeAreaView>
     )
 }
--- src/tsx/modal.tsx
+++ src/tsx/modal.tsx
@@ -1,44 +1,68 @@
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
-import { StyleSheet, View, Text, TextInput } from "react-native";
+import { Modal, StyleSheet, View, Text, TextInput, Pressable } from "react-native";
 
 export interface Titles{
     descricao: string,
     type: string,
+    isVisible: boolean,
+    onClose: () => void,
 }
 
-export default function Modal(props: Titles){
+export default function CustomModal(props: Titles){
     return(
-        <View
-        style={styles.container}>
-            <Text>
-                {props.type}
-            </Text>
-            <View
-        style={styles.modal}>
-                <Text>
-                </Text>
-                <TextInput
-                    style={styles.input}
-                    // value={number}
-                    placeholder="95 km"
-                    keyboardType="numeric"
-                />
-                <Text>{props.descricao}</Text>
-            </View>
-        </View>
+        <Modal
+            animationType='slide'
+            transparent={true}
+            visible={props.isVisible}
+            style={styles.modal}
+            >
+                <View
+                    style={styles.container}>
+                    <View
+                        style={styles.title}>
+                        <Text
+                        style={{color:'#cfcfcf',
+                            fontSize: 20}}>
+                            {props.descricao}
+                        </Text>
+                        <Pressable
+                            onPress={props.onClose}>
+                            <MaterialCommunityIcons
+                                name="close"
+                                color="#cfcfcf"
+                                size={20} />
+                        </Pressable>
+                    </View>
+                    <View
+                    style={styles.content}>
+                        <Text>
+                            Modal
+                        </Text>
+                    </View>
+                </View>
+        </Modal>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex:1,
         alignItems: 'center',
-        backgroundColor: 'white',
-        justifyContent: 'center',
+        bottom: 0,
+        backgroundColor: '#cfcfcf',
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        // justifyContent: 'center',
+        width: '100%',
+        height: '75%',
         position: 'absolute',
+    },
+    content:{
+        
     },
     modal:{
         elevation: 5,
+        backgroundColor: 'blue',
         // paddingHorizontal: 75,
         // margin: 20,
         // alignContent: 'center',
@@ -51,7 +75,16 @@ const styles = StyleSheet.create({
         borderColor: 'black',
         borderBottomColor: 'gray',
         elevation: 3,
-    }
-}
-
-)
+    },
+    title:{
+        backgroundColor: '#005500',
+        height: '10%',
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        width: '100%',
+        paddingHorizontal: 20,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+    },
+})
