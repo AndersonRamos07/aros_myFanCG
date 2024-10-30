06ee2a08d4de9c4d261810a0aa3c8d6c4231e57f
Anderson Ramos <andersonramos07@live.com>
2024-10-29 22:50:47 -0300

    adjusting odometer

--- src/tsx/odometer.tsx
+++ src/tsx/odometer.tsx
@@ -1,16 +1,20 @@
 import { StyleSheet, View , Text } from "react-native";
 
-export default function Hodometro()
+export interface Aspect{
+    wBorder: boolean,
+    kmOdometer: number[],
+}
+export default function Hodometro(props: Aspect)
 {
     return(
-        <View style={styles.borda}>
+        <View style={props.wBorder? styles.borda : null}>
             <View style={styles.odometro}>
-                <Text style={[styles.kms, styles.texts]}>8</Text>
-                <Text style={[styles.kms, styles.texts]}>8</Text>
-                <Text style={[styles.kms, styles.texts]}>8</Text>
-                <Text style={[styles.kms, styles.texts]}>8</Text>
-                <Text style={[styles.kms, styles.texts]}>8</Text>
-                <Text style={[{backgroundColor: 'white'}, styles.texts]}>8</Text>
+                <Text style={[styles.kms, styles.texts]}>{props.kmOdometer[0]}</Text>
+                <Text style={[styles.kms, styles.texts]}>{props.kmOdometer[1]}</Text>
+                <Text style={[styles.kms, styles.texts]}>{props.kmOdometer[2]}</Text>
+                <Text style={[styles.kms, styles.texts]}>{props.kmOdometer[3]}</Text>
+                <Text style={[styles.kms, styles.texts]}>{props.kmOdometer[4]}</Text>
+                <Text style={[{backgroundColor: 'white'}, styles.texts]}>{props.kmOdometer[5]}</Text>
             </View>
         </View>
     )
