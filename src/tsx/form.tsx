341a09be92d440f7731868d754d4f33cb3387630
Anderson Ramos <andersonramos07@live.com>
2024-11-06 10:53:53 -0300

    setting the form

--- src/tsx/form.tsx
+++ src/tsx/form.tsx
@@ -1,24 +1,52 @@
-import { useForm } from 'react-hook-form';
+import { Text, View, TextInput, Button, Alert } from "react-native";
+import { useForm, Controller } from "react-hook-form";
 
-export default function Form()
-{
-    const {
-        register,
-        handleSubmit
-    } = useForm();
-    const onSubmit = (data) => console.log(data);
-    return(
-        <>
-        <form
-            onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("firstName")} />
-            <select {...register("gender")}>
-                <option value="female">female</option>
-                <option value="male">male</option>
-                <option value="other">other</option>
-            </select>
-            <input type="submit" />
-        </form>
-        </>
-    )
+export default function Form() {
+  const { control, handleSubmit, formState: { errors } } = useForm({
+    defaultValues: {
+      kmNewValue: '',
+      addService: ''
+    }
+  });
+  const onSubmit = data => console.log(data);
+
+  return (
+    <View>
+      <Controller
+        control={control}
+        rules={{
+         required: true,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="-------km-------"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+            keyboardType="numeric"
+          />
+        )}
+        name="kmNewValue"
+      />
+      {errors.kmNewValue && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        rules={{
+         maxLength: 100,
+        }}
+        render={({ field: { onChange, onBlur, value } }) => (
+          <TextInput
+            placeholder="Service"
+            onBlur={onBlur}
+            onChangeText={onChange}
+            value={value}
+          />
+        )}
+        name="addService"
+      />
+
+      <Button title="Submit" onPress={handleSubmit(onSubmit)} />
+    </View>
+  );
 }
