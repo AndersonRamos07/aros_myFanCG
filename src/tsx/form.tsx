95d1bfd23c5b5077ccfbb5c26e00e91a93f3112a
Anderson Ramos <andersonramos07@live.com>
2024-11-06 23:06:39 -0300

    fixing the form

--- src/tsx/form.tsx
+++ src/tsx/form.tsx
@@ -1,14 +1,19 @@
 import { Text, View, TextInput, Button, Alert } from "react-native";
 import { useForm, Controller } from "react-hook-form";
+import SelectDropdown from 'react-native-select-dropdown';
+
+import services from '../../assets/data_Select';
+import { useState } from "react";
 
 export default function Form() {
+  const [ servico , setServico ] = useState('');
   const { control, handleSubmit, formState: { errors } } = useForm({
     defaultValues: {
       kmNewValue: '',
-      addService: ''
+      newService: '',
     }
   });
-  const onSubmit = data => console.log(data);
+  const onSubmit = (data: any) => console.log(data);
 
   return (
     <View>
@@ -29,23 +34,45 @@ export default function Form() {
         name="kmNewValue"
       />
       {errors.kmNewValue && <Text>This is required.</Text>}
-
+          <SelectDropdown
+            data={services}
+            onSelect={(item: any, index: any)=> setServico(item.description)}
+            renderButton={(a: any, b: any) => {
+                return (
+                  <View>
+                    <Text>{(a && b) || 'Adicionar servico'}</Text>
+                  </View>
+                )
+            }}
+            renderItem={(item: any, index: any, isSelected: any) => {
+              const now = (a: any) => setServico(a)
+              now(item.title)
+              console.log('servico: '+servico)
+              return (
+                <View>
+                  <Text>{item.title}</Text>
+                </View>
+              );
+            }}
+          />
       <Controller
         control={control}
         rules={{
-         maxLength: 100,
+         required: true,
         }}
         render={({ field: { onChange, onBlur, value } }) => (
+          value = servico,
           <TextInput
-            placeholder="Service"
+            placeholder="servico"
             onBlur={onBlur}
             onChangeText={onChange}
             value={value}
+            // keyboardType=""
           />
         )}
-        name="addService"
+        name="newService"
       />
-
+      {errors.newService && <Text>which one - - - This is required.</Text>}
       <Button title="Submit" onPress={handleSubmit(onSubmit)} />
     </View>
   );
