23241d117e4976767212d252c6029a369f0f9002
Anderson Ramos <andersonramos07@live.com>
2025-03-27 18:49:59 -0400

    build: trying to refactor with java

---
+++ mfc_java/Main.java
@@ -0,0 +1,22 @@
+public class Main{
+    public static void main(String[] args)
+    {
+        /*
+        //TODO:
+
+            input {
+                apps utilizados, String
+                valores em reais, R$ double
+                km de inicio, double km
+                km de termino, double km
+            }
+            output {
+                total em R$, R$ double
+                total em km, double km
+                total de horas, data h
+                R$/km, R$ double /km
+                R$/h, R$ double /h
+            }
+        */
+    }
+}
